812b9edec4761864e55fc4f5378075e9c91cb521
Paperzlel <lpitchfork@hotmail.com>
2024-02-19 17:57:32 +0000

    0.2.1 - minor refactor
    Finally re-did all of the app in Python, plus some safety things so that it doesn't crash (hopefully)
    - Loading more than one set of coordinates
    - Saving any amount of coords to one place
    - Removing the saved.txt file from the repo, and the option to remove it in-app
    - Erasing all the data from the file also exists
    - Updated fancy UI to have all the options!
    - Probably a comment I forgot to remove

--- src/line_coords.py
+++ src/line_coords.py
@@ -1,7 +1,8 @@
 import re
 import time
+import os
 
-def options():
+def options() -> bool:
     selection = input("")
     if selection == '1':
         set_coordinates()
@@ -10,6 +11,12 @@ def options():
         load_coords()
         return True
     elif selection == '3':
+        clear_file()
+        return True
+    elif selection == '4':
+        remove_file()
+        return True
+    elif selection == '5':
         print("quitting...")
         return False
     else: 
@@ -22,21 +29,29 @@ def main_menu():
         time.sleep(1)
         print("Welcome to LineGraph!")
         print("Please choose an option: ")
-        print("1. Set coordinates")
-        print("2. Load coordinates from save")
-        print("3. Quit")
+        print("1. Add a set of coordinates to the file")
+        print("2. Load the sets of coordinates from a save")
+        print("3. Erase all coordinates from the save")
+        print("4. Delete the savefile")
+        print("5. Quit")
         options_loop = options()
 
 
 def check_str_for_coords(string):
     # google python regex when you need this again
-    ans = re.search("^(.*[0-9].*,.*[0-9].*)$", string)
-    print(ans)
-    if ans:
-        print("String is a set of coordinates with numbers \n")
-        parse_line_as_x_and_y(string)
+    ans = re.findall("^(.*[0-9].*,.*[0-9].*)$", string)
+    if ans == None:
+        print("ERROR: Line is null")
     else:
-        print("ERROR: String doesn't contain a set of coordinates \n")
+        if len(ans) > 1:
+            print("DEBUG: More than one coordinate in line found!")
+            for i in range (len(ans)):
+                print(ans[i])
+        elif len(ans) == 0:
+            print("ERROR: Coordinates don't exist!")
+        else:
+            print(ans[0])
+            parse_line_as_x_and_y(string)
     
 
 def make_string_of_vars(x, y):
@@ -47,34 +62,49 @@ def make_string_of_vars(x, y):
     print(coordinate_str)
 
 def set_coordinates():
-    x = input("Please enter an x value: \n")
-    y = input("Please enter a y value: \n")
-    make_string_of_vars(x, y)
-    open_file_and_set_file_type("w", coordinate_str)
+    second_x = input("Please enter an X value: \n")
+    second_y = input("Please enter a Y value: \n")
+    make_string_of_vars(second_x, second_y)
+    open_file_and_set_file_type("a", coordinate_str)
 
 def load_coords():
     try:
-        file = open("saved.txt", "x")
+        file = open("saved.txt", "r")
     except:
-        print("DEBUG: File already exists")
+        print("ERROR: File doesn't exist!")
     else:
-        open_file_and_set_file_type("r", None)
-    finally:
         open_file_and_set_file_type("r", None)
 
 def parse_line_as_x_and_y(line):
-    obj = re.findall(r"\d+", line)
-    x = obj[0]
-    y = obj[1]
     # Add z here when doing 3D
-    make_string_of_vars(x, y)
+    obj = re.findall(r"\d+", line)
+    if len(obj) % 2 != 0:
+        print("ERROR: Coordinates are in an invalid quantity!")
+    else:
+        for i in range(len(obj)):
+            if len(obj) % 2 == 0:
+                x = obj[i]
+            else:
+                y = obj[i]
+                make_string_of_vars(x, y)
 
 def open_file_and_set_file_type(type, text):
     with open("saved.txt", type) as file:
         if type == 'r':
             check_str_for_coords(file.readline())
-        elif type == 'w':
+        elif type == 'a':
             file.write(text)
         file.close()
-        
+
+def remove_file():
+    if os.path.exists("saved.txt"):
+        os.remove("saved.txt")
+    else:
+        print("ERROR: File does not exist!")
+
+def clear_file():
+    with open("saved.txt", 'w') as file:
+        file.write("")
+        file.close()
+
 main_menu()
